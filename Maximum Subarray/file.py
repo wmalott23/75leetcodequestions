fa7b47504850174f55c28228a5f93619add0031f
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-06-24 13:56:24 -0700

    Completed question 5 (faster runtime than 98.38%)!

--- Maximum Subarray/file.py
+++ Maximum Subarray/file.py
@@ -1,6 +1,14 @@
 class Solution(object):
     def maxSubArray(self, nums):
-
+        high = nums[0]
+        current = 0
+        for x in range(len(nums)):
+            if current < 0:
+                current = 0
+            current += nums[x]
+            if current > high:
+                high = current
+        return high
 
 run = Solution()
 print(run.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
